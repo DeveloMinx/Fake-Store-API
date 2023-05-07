f77de46c956037fae7842d94755151cffa3442fd
MinaCode24 <anderson.mina.ix98@gmail.com>
2023-05-06 19:17:11 -0500

    Se agrego la nav y el footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,20 @@ import { AppComponent } from './app.component';
 import { MainComponent } from './components/main/main.component';
 import { CardsComponent } from './components/cards/cards.component';
 import {HttpClientModule} from '@angular/common/http';
-import { PrincipalComponent } from './components/principal/principal.component'
+import { PrincipalComponent } from './components/principal/principal.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { NavbarMovileComponent } from './components/navbar-movile/navbar-movile.component';
+import { FooterComponent } from './components/footer/footer.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     MainComponent,
     CardsComponent,
-    PrincipalComponent
+    PrincipalComponent,
+    NavbarComponent,
+    NavbarMovileComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/navbar-movile/navbar-movile.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar-movile',
+  templateUrl: './navbar-movile.component.html',
+  styleUrls: ['./navbar-movile.component.scss']
+})
+export class NavbarMovileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
